73920627f897321a966ae742ac5de942855d4e2a
Rounak77382 <rounaks255@gmail.com>
2024-12-19 17:12:49 +0530

    vercel

--- app/_lib/utils.js
+++ app/_lib/utils.js
@@ -1,6 +1,76 @@
+// app/_lib/utils.js
 import snoowrap from "snoowrap";
+import dotenv from "dotenv";
+import fetch from "node-fetch";
+import NodeCache from "node-cache";
 import qs from "qs";
 import { getGif } from "../_components/Red";
+
+dotenv.config();
+
+const cache = new NodeCache({ stdTTL: 3600 }); // Cache TTL set to 1 hour
+
+async function getReadOnlyAccessToken() {
+  const response = await fetch("https://www.reddit.com/api/v1/access_token", {
+    method: "POST",
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from(
+          `${process.env.praw_api_client_id}:${process.env.praw_api_client_secret}`
+        ).toString("base64"),
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: "grant_type=client_credentials",
+  });
+
+  const data = await response.json();
+  return data.access_token;
+}
+
+async function getStoredRefreshToken() {
+  const token = cache.get("refreshToken");
+  return token || null;
+}
+
+async function storeRefreshToken(token) {
+  cache.set("refreshToken", token);
+}
+
+async function validateToken(token) {
+  try {
+    const response = await fetch("https://oauth.reddit.com/api/v1/me", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.ok;
+  } catch (error) {
+    return false;
+  }
+}
+
+export async function listSubreddits(subredditName) {
+  let accessToken = await getStoredRefreshToken();
+
+  if (!accessToken || !(await validateToken(accessToken))) {
+    accessToken = await getReadOnlyAccessToken();
+    await storeRefreshToken(accessToken);
+  }
+
+  const r = new snoowrap({
+    userAgent: "testscript by RS_ted",
+    clientId: process.env.praw_api_client_id,
+    clientSecret: process.env.praw_api_client_secret,
+    accessToken: accessToken,
+  });
+
+  const subredditResults = await r.searchSubredditNames(
+    { query: subredditName },
+    { includeNsfw: true }
+  );
+  return subredditResults;
+}
 
 async function getAccessToken() {
   const authString = `${process.env.praw_api_client_id}:${process.env.praw_api_client_secret}`;
@@ -106,7 +176,6 @@ export async function* downloadImages(
         Posts = await subreddit.getTop({ time: since, limit });
     }
   }
-
 
   for (const post of Posts) {
     if (signal && signal.aborted) {
