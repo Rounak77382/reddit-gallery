4989c5c3c6485ec6e15fe473186182d3c16121ad
Rounak77382 <rounaks255@gmail.com>
2024-12-19 20:53:45 +0530

    Disable Static Chunking

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: false };
+const nextConfig = {
+  reactStrictMode: false,
+  webpack(config) {
+    config.optimization.splitChunks = false; // Disable code splitting
+    return config;
+  },
+};
 
 export default nextConfig;
